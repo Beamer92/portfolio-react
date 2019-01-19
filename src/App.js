5e7be95acb335eb9a9029f627f2b6bcefc6168f9
Brendan Woodell <brendan.woodell@gmail.com>
2019-01-19 10:58:30 -0800

    general Component structure done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router'
 import Home from './Home'
+import Project from './Project'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    state = {
+      projects: []
+    }
+  }
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path='/' />
-          <Route path='/' />
-          <Route path='/' />
+          <Route path='/' Component={Home}/>
+          <Route path='/p1' render={(props) => <Project props={this.state.projects[0]}/>}/>
+          <Route path='/p2' render={(props) => <Project props={this.state.projects[1]}/>}/>
+    <Route path='/p3' render={(props) => <Project props={this.state.projects[2]}/>}/>
         </Switch>
       </BrowserRouter>
     );
